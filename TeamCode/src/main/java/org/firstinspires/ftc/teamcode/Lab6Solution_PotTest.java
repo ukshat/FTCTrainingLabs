6022ce6e5dffe67edb4f88107fd764b99946c5cd
Siddharth Kadari <skadari270@student.fuhsd.org>
2021-07-21 12:16:47 -0700

    Lab 6

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lab6Solution_PotTest.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+
+@Autonomous(name = "Potentiometer Test")
+public class Lab6Solution_PotTest extends LinearOpMode {
+    private AnalogInput pot; //Define AnalogInput, which we use to read the voltage from the potentiometer
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        pot = hardwareMap.analogInput.get("testPot"); //Map a configured Analog Device (potentiometer) to an object in Java
+
+        waitForStart();
+
+        while(opModeIsActive()){ //Loop as long as the opmode has not ended
+            telemetry.addData("Voltage", pot.getVoltage()); //pot.getVoltage() reads the voltage recieved from the potentiometer
+            telemetry.update();
+
+            sleep(50);
+        }
+
+        stop();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lab6Template_PotTest.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+
+@Autonomous(name = "Potentiometer Test")
+public class Lab6Template_PotTest extends LinearOpMode {
+    private AnalogInput pot; //Define AnalogInput, which we use to read the voltage from the potentiometer
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //This time, we did not provide the code to hardware map the analog devce.
+        //From this lab forward, you must figure out how to hardwaremap the devices you will be using in your labs and robot
+        //Observe previous labs and try to figure out hardware mapping from those
+
+        //YOUR CODE HERE
+
+    }
+}
