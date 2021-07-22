994ffa24be87d16d69fc643f86d53a1c59205192
Siddharth Kadari <skadari270@student.fuhsd.org>
2021-07-21 17:07:20 -0700

    Lab 9

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lab09Template_LEDTest.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+
+@Autonomous(name = "Color Test")
+public class Lab09Template_LEDTest extends LinearOpMode {
+
+    private DigitalChannel redLed, greenLed;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        //YOUR CODE HERE
+
+    }
+}
