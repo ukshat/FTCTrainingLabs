47757075402e4821f6ea5a0fb7ac2e99538e5511
ukshat <33008157+ukshat@users.noreply.github.com>
2021-07-21 23:34:53 -0800

    Fixed servo setPosition

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lab04Solution_ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lab04Solution_ServoTest.java
@@ -13,16 +13,16 @@ public class Lab04Solution_ServoTest extends LinearOpMode {
         servo = hardwareMap.servo.get("testServo"); //Map a configured Servo motor to an object in Java
         servo.setDirection(Servo.Direction.FORWARD); //Ensure the servo object defaults to the same direction every time
 
-        servo.setPosition(135); //setPosition
+        servo.setPosition(0.375); // setPosition only takes a number from 0 to 1, so we have to map the 135° to this range. 135/360 is 0.375.
 
         waitForStart();
 
         for(int i = 0; i < 6; i++){ //repeat x6
-            servo.setPosition(0); //setPosition
+            servo.setPosition(0); // set to 0°
 
             sleep(3000); //blocks the code from executing for 3000ms (3000ms = 3s)
 
-            servo.setPosition(270); //setPosition
+            servo.setPosition(0.75); // set to 270° (270/360 = 0.75)
 
             sleep(5000); //sleep 5s
         }
