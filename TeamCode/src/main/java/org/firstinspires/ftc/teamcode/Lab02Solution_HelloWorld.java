ea65db93314d7fa85ab9e89812f7515c92aa67a7
ukshat <33008157+ukshat@users.noreply.github.com>
2021-07-21 18:59:37 -0700

    Use addLine() rather than addData()

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lab02Solution_HelloWorld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lab02Solution_HelloWorld.java
@@ -9,7 +9,7 @@ public class Lab02Solution_HelloWorld extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         waitForStart(); //Self explanatory
 
-        telemetry.addData("", "Hello World"); //Add data to telemetry queue
+        telemetry.addLine("Hello World"); //Add data to telemetry queue
         telemetry.update(); //Update telemetry queue, pushes "Hello World" to the driver station
 
         //This is to allow the operator to actually see the Hello World message, without this line, the program would immediately terminate, and therefore the operator would never see the telemetry data in time
