bcc223fec1b3c0ff43d1bb2d89f2146a39b6b9ae
Siddharth Kadari <skadari270@student.fuhsd.org>
2021-07-27 12:28:23 -0700

    Lab 12 lame fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EasyCVTest1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EasyCVTest1.java
@@ -2,21 +2,8 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.imgproc.Imgproc;
-import org.openftc.easyopencv.OpenCvCamera;
-import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
-import org.openftc.easyopencv.OpenCvPipeline;
-import org.opencv.core.Scalar;
-
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 
 import CupertinoRobotics.support.wrapper.EasyCV.EasyCV;
 
