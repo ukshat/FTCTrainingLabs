30da12261f53000d517eba362c7130e8b871886d
Siddharth Kadari <skadari270@student.fuhsd.org>
2021-07-29 12:58:42 -0700

    Slight Hardware Wrapper Restructure

--- TeamCode/src/main/java/CupertinoRobotics/support/Hardware/DigitalChannelLED/DigitalChannelLED.java
+++ TeamCode/src/main/java/CupertinoRobotics/support/Hardware/DigitalChannelLED/DigitalChannelLED.java
@@ -6,45 +6,42 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public final class DigitalChannelLED {
     private final DigitalChannel rLED, gLED;
+    private LEDState state;
 
     public DigitalChannelLED(HardwareMap hw, String redDeviceName, String greenDeviceName){
         rLED = hw.digitalChannel.get(redDeviceName);
         gLED = hw.digitalChannel.get(greenDeviceName);
         rLED.setMode(DigitalChannel.Mode.OUTPUT);
         gLED.setMode(DigitalChannel.Mode.OUTPUT);
-    }
-
-    public enum LEDState{
-        RED,
-        GREEN,
-        AMBER,
-        NONE
+        setState(LEDState.NONE);
     }
 
     public final void setState(LEDState state){
         rLED.setState(state == LEDState.AMBER || state == LEDState.RED);
         gLED.setState(state == LEDState.AMBER || state == LEDState.GREEN);
+        this.state = state;
     }
 
-    public final void closeRed(){ rLED.close(); }
+    public final LEDState getState(){
+        return state;
+    }
 
-    public final void resetDeviceConfigurationForOpModeRed() { rLED.resetDeviceConfigurationForOpMode(); }
+    public final void close(){ rLED.close(); gLED.close(); }
+
+    public final void resetDeviceConfigurationForOpMode() { rLED.resetDeviceConfigurationForOpMode();  gLED.resetDeviceConfigurationForOpMode(); }
 
     public final int getVersionRed() { return rLED.getVersion(); }
 
-    public final String getDeviceNameRed() { return rLED.getDeviceName(); }
+    public final String getRedDeviceName() { return rLED.getDeviceName(); }
 
     public final String getConnectionInfoRed() { return rLED.getConnectionInfo(); }
 
     public final HardwareDevice.Manufacturer getManufacturerRed() { return rLED.getManufacturer(); }
 
-    public final void closeGreen(){ gLED.close(); }
-
-    public final void resetDeviceConfigurationForOpModeGreen() { gLED.resetDeviceConfigurationForOpMode(); }
 
     public final int getVersionGreen() { return gLED.getVersion(); }
 
-    public final String getDeviceNameGreen() { return gLED.getDeviceName(); }
+    public final String getGreenDeviceName() { return gLED.getDeviceName(); }
 
     public final String getConnectionInfoGreen() { return gLED.getConnectionInfo(); }
 
---
+++ TeamCode/src/main/java/CupertinoRobotics/support/Hardware/DigitalChannelLED/LEDState.java
@@ -0,0 +1,12 @@
+package CupertinoRobotics.support.Hardware.DigitalChannelLED;
+
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.HardwareDevice;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public enum LEDState{
+    RED,
+    GREEN,
+    AMBER,
+    NONE
+}
