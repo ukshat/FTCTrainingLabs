aeb95fdebb946777d1b1a3a49dd6dcf7d82c3eef
ukshat <33008157+ukshat@users.noreply.github.com>
2021-08-14 08:07:46 -0700

    Added axis option to Gyro

--- TeamCode/src/main/java/CupertinoRobotics/support/Hardware/Gyro.java
+++ TeamCode/src/main/java/CupertinoRobotics/support/Hardware/Gyro.java
@@ -13,19 +13,44 @@ public class Gyro {
 
     private AngleUnit unit;
 
+    enum Axis { X, Y, Z }
+
+    private Axis axis;
+
     public Gyro(HardwareMap hardwareMap) {
         imu = hardwareMap.get(BNO055IMU.class, "imu");
         imu.initialize(new BNO055IMU.Parameters());
+        imu.getParameters().angleUnit = BNO055IMU.AngleUnit.DEGREES;
         setUnit(AngleUnit.RADIANS);
+        setAxis(Axis.Z);
     }
 
     public Gyro(HardwareMap hardwareMap, AngleUnit unit) {
         imu = hardwareMap.get(BNO055IMU.class, "imu");
         imu.initialize(new BNO055IMU.Parameters());
         setUnit(unit);
+        setAxis(Axis.Z);
+    }
+
+    public Gyro(HardwareMap hardwareMap, Axis axis) {
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(new BNO055IMU.Parameters());
+        setUnit(AngleUnit.RADIANS);
+        setAxis(axis);
+    }
+
+    public Gyro(HardwareMap hardwareMap, AngleUnit unit, Axis axis) {
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(new BNO055IMU.Parameters());
+        setUnit(unit);
+        setAxis(axis);
     }
 
     public double getAngle() {
+        if (axis == Axis.X)
+            return imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, unit).firstAngle;
+        if (axis == Axis.Y)
+            return imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.YXZ, unit).firstAngle;
         return imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, unit).firstAngle;
     }
 
@@ -35,6 +60,14 @@ public class Gyro {
 
     public void setUnit(AngleUnit unit) {
         this.unit = unit;
+    }
+
+    public void setAxis(Axis axis) {
+        this.axis = axis;
+    }
+
+    public Axis getAxis() {
+        return axis;
     }
 
 }
